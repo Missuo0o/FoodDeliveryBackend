b14b623070a420f4e5a356320726da5c9bb31d69
Shun Zhang <sz4297@nyu.edu>
2024-01-29 04:54:33 -0500

    feat:some basic functions

--- src/main/java/com/missuo/server/service/DishService.java
+++ src/main/java/com/missuo/server/service/DishService.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface DishService {
 
-  public void saveWithFlavor(DishDTO dishDTO);
+  void saveWithFlavor(DishDTO dishDTO);
 
   PageResult<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 
