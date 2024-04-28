ae6f012c38907cd0c92c78c94dca30b2a0eb6ef5
Shun Zhang <sz4297@nyu.edu>
2024-04-28 06:26:41 -0400

    fix: fix bugs

--- src/test/java/com/missuo/MissuoApplicationTests.java
+++ src/test/java/com/missuo/MissuoApplicationTests.java
@@ -1,6 +1,13 @@
-// package com.missuo;
-//
-// import org.springframework.boot.test.context.SpringBootTest;
-//
-// @SpringBootTest
-// public class MissuoApplicationTests {}
+package com.missuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MissuoApplicationTests {
+
+  @Test
+  public void testSimpleMessage() {
+    System.out.println("This is a simple test message.");
+  }
+}
